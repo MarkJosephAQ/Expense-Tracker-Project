74ef4853b7c91bba66fe1e911ef7726c3a52617b
Mark Joseph Almazan Aquino <markjosephaquino_a@yahoo.com>
2024-09-25 15:09:21 +0800

    initializing project

---
+++ src/test/java/ExpenseTracker/Expense/tracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ExpenseTracker.Expense.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
